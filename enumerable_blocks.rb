db56508193736651617bf6f73e7fca5b559f0ba4
Josias Alvarado <josiasjag@gmail.com>
2020-11-03 11:53:46 -0600

    Update #my_each method.

--- enumerable_blocks.rb
+++ enumerable_blocks.rb
@@ -1,45 +1,53 @@
 module Enumerable
   # my_each
-  def my_each(arr)
-    arr.each do |i|
-      yield i
+  def my_each
+    to_enum(:my_each) unless block_given?
+    if self.instance_of?(Array)
+      element = self
+    else
+      element = to_a
     end
+
+    k = 0
+    while k < element.length
+      yield element[k]
+      k += 1
+    end
+    self
   end
 
   # my_each_with_index
-  def my_each_with_index(arr)
-    i = 0
-    while i < arr.length
-      yield i, arr[i]
-      i += 1
+  def my_each_with_index
+    to_enum(:my_each_with_index) unless block_given?
+    element = to_a
+    k = 0
+    while k < element.length
+      yield element[k], k
+      k += 1
     end
+    self
   end
 
   # my_select
-  def my_select(arr)
-    i = 0
-    while i < arr.length
-      yield arr[i]
-      i += 1
-    end
-  end
-
-  my_select([1, 2, 3, 4, 5, 6, 7, 8]) do |elem|
-    puts elem if elem.even?
+  def my_select
+    to_enum(:my_select) unless block_given?
+    arr = Array.new
+    my_each {|i| arr << i if yield i}
+    arr
   end
 
   # my_all?
-  def my_all(&my_block)
+  def my_all?(&my_block)
     my_block.call
   end
 
   # my_any?
-  def my_any(_arr, _name)
+  def my_any?(_arr, _name)
     yield
   end
 
   # my_none?
-  def my_none(arr)
+  def my_none?(arr)
     arr.each do |i|
       yield i
     end
@@ -47,7 +55,7 @@ module Enumerable
 
   def my_count(param = nil)
     count = 0
-    :my_count.lenght unless block_given?
+    :my_count.length unless block_given?
     if param.instance_of?(Number)
       each do |element|
         count += 1 if element == param
@@ -71,9 +79,9 @@ module Enumerable
   end
 
   def my_inject(param = nil, symbol = nil)
-    if (!param.nil? && symbol.nil?) && (param.instance_of?(Symbol) || param.instance_of?(String))
+    if (!param.nil? && symbol.nil?) && (param.is_a?(Symbol) || param.is_a?(String))
       # If the param is not nil and the symbol is not nil and param is a symbol or param is a string:
-      symbol = argument # symbol is now param
+      symbol = param # symbol is now param
       param = nil # param is now nil
     end
     if !block_given? && !symbol.nil? # If no block and symbol is not nil
@@ -84,17 +92,8 @@ module Enumerable
     end
     param
   end
+
+  def multiply_els(items)
+    items.my_inject { |result, item| result * item }
+  end
 end
-
-p my_each(%w[a b c d e]) { |elem| puts elem.upcase }
-my_each_with_index(%w[ruby css takhmina josias microverse]) { |index, elem| puts "#{index}, #{elem}" }
-
-arr = [1, 2, 3, 4, 5, 6, 7, 8]
-my_all do
-  puts arr.is_a?(String)
-end
-
-array = %w[Josias Mina Microverse]
-nm = 'Mina'
-my_any(array, nm) { p array.include?(nm) }
-my_none(%w[bacon orange apple]) { |k| puts false unless k.size == 5 }
